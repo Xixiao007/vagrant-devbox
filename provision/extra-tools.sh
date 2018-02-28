b12e4334ef7e3448f9f9f711b12481e2e838485f
Xi Xiao <xi.xiao@biit.fi>
2018-02-28 13:15:43 +0200

    +

--- provision/extra-tools.sh
+++ provision/extra-tools.sh
@@ -6,14 +6,6 @@ temp_path=${vagrant_path}/temp
 mkdir -p ${vagrant_path}/temp
 cd ${temp_path}
 
-# oracle java8 - It is in post action because the license accept window cannot passed by in vagrant
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get update -y
-sudo apt-get install oracle-java8-installer -y
-
-# ant
-sudo apt-get install ant -y
-
 # Ecplise TO-DO
 # curl -k https://raw.githubusercontent.com/budhash/install-eclipse/master/install-eclipse > install-eclipse; chmod +x install-eclipse
 # ./install-eclipse -p "https://developer.salesforce.com/media/force-ide/eclipse45" eclipse
--- provision/post_action.sh
+++ provision/post_action.sh
@@ -17,6 +17,11 @@ fisher fnm
 fnm 8
 npm install sfdx-cli --global
 
+# oracle java8 - It is in post action because the license accept window cannot passed by in vagrant
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update -y
+sudo apt-get install oracle-java8-installer -y
+
 # ant
 sudo apt-get install ant -y
 
